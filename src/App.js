367fb504a307fbbc29660f8d110101d137420657
Dave Laffan <dave.laffan@outlook.com>
2020-06-09 13:16:59 +0100

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            monsters: [],
+            searchField: "",
+        };
+
+        // arrow func with handleChange removes the need to bind 'this'
+        // this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => this.setState({ monsters: users }));
+    }
+
+    handleChange = e => {
+        this.setState({ searchField: e.target.value });
+    };
+
+    render() {
+        const { monsters, searchField } = this.state;
+        // This is same as:
+        // const monsters = this.state.monsters - and same again for searchField
+
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+
+        /* jshint ignore:start */
+        return (
+            <div className="App">
+                <SearchBox
+                    placeholder="search robots"
+                    handleChange={this.handleChange}
+                />
+                {/*
+                    monsters is a props object that is being
+                    passed to CardList component.
+                */}
+                <CardList monsters={filteredMonsters} />
+            </div>
+        );
+        /* jshint ignore:end */
+    }
 }
 
 export default App;
